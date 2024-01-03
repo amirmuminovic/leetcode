f1a90a173f0ea861fdbfe05447a9d0c9d7ce3ad3
Amir Muminović <37328370+amirmuminovic@users.noreply.github.com>
2024-01-03 23:12:24 +0100

    Add 2610

---
+++ 2610-array-count-counversion/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<int> freq = vector<int>(nums.size()+1);
+
+        vector<vector<int>> ans;
+        for (int x: nums) {
+            if (freq[x] >= ans.size()) {
+                ans.push_back({});
+            }
+            ans[freq[x]].push_back(x);
+            ++freq[x];
+        }
+
+        return ans;
+    }
+};
