1eb06be84241785f28e47a99b3b01262c7a7a364
Amir Muminović <37328370+amirmuminovic@users.noreply.github.com>
2024-01-10 10:13:07 +0100

    Add 872 Leaf Similar Tree

---
+++ 872-leaf-similar-trees/Solution.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void getLeaves(TreeNode* root, vector<int> &v) {
+        if (!root->left && !root->right) {
+            v.push_back(root->val);
+        } else {
+            if (root->left)
+                getLeaves(root->left, v);
+            if (root->right)
+                getLeaves(root->right, v);
+        }
+    }
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> leaves1;
+        getLeaves(root1, leaves1);
+        vector<int> leaves2;
+        getLeaves(root2, leaves2);
+
+        if (leaves1.size() != leaves2.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < leaves1.size(); ++i) {
+            if (leaves1[i] != leaves2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
